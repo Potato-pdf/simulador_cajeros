23c6615aad83290f6dddc9366e15104df46719b1
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-10-27 14:43:33 -0600

    feat: db connections

--- banco1/backend/src/db/db_read_conection.ts
+++ banco1/backend/src/db/db_read_conection.ts
@@ -3,13 +3,13 @@ import path from "path";
 
 sqlite3.verbose();
 
-const db_read_path = path.resolve(__dirname, "../../database/read.db");
+const db_path = path.resolve(__dirname, "../../database/db.db");
 
-export const db_read_connection = new sqlite3.Database(db_read_path, (err) => {
+export const db_read_connection = new sqlite3.Database(db_path, (err) => {
   if (err) {
-    console.error("Error connecting to the read database:", err.message);
+    console.error("Error connecting to the database:", err.message);
   } else {
-    console.log("Connected to the read SQLite database.");
+    console.log("Connected to the SQLite database.");
   }
 });
 
--- banco1/backend/src/db/db_write_connection.ts
+++ banco1/backend/src/db/db_write_connection.ts
@@ -3,11 +3,11 @@ import path from "path";
 
 sqlite3.verbose();
 
-const db_write_path = path.resolve(__dirname, "../../database/write.db");
+const db_path = path.resolve(__dirname, "../../database/db.db");
 
-export const db_write_connection = new sqlite3.Database(db_write_path, (err) => {
+export const db_write_connection = new sqlite3.Database(db_path, (err) => {
   if (err) {
-    console.error("Error connecting to the write database:", err.message);
+    console.error("Error connecting to the database:", err.message);
   } else {
     console.log("Connected to the write SQLite database.");
   }
